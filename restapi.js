36ed6f0c17472255af7b4f3ad1d20c8d8981fb8d
nicoonievas <nicolasnievas1@gmail.com>
2024-05-30 18:16:51 -0300

    segunda subida

--- restapi.js
+++ restapi.js
@@ -48,6 +48,51 @@ app.get('/tempget', async (req, res) => {
 
 
 
+// Verificar el token JWT
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+
+
+
+//Genera Token
+const generateToken = (user) => {
+  return jwt.sign(user, JWT_SECRET, { expiresIn: '1h' });
+};
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  //Lógica de user y pass
+  if (username === 'raul' && password === '1234') {
+    const token = generateToken({ username });
+    res.json({ token }); //Devuelve token con 1h. de validez
+  } else {
+    res.status(401).json({ error: 'Credenciales incorrectas' });
+  }
+});
+
+
+
+
+
+
+
+
 
 app.listen(PORT3, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT3}`);
